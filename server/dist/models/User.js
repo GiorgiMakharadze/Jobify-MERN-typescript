f7a182d4562a145792a867e8fecce00e91078630
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-04-26 23:02:48 +0400

    deleted key

--- server/dist/models/User.js
+++ server/dist/models/User.js
@@ -1,99 +1,152 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        var desc = Object.getOwnPropertyDescriptor(m, k);
+        if (
+          !desc ||
+          ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)
+        ) {
+          desc = {
+            enumerable: true,
+            get: function () {
+              return m[k];
+            },
+          };
+        }
+        Object.defineProperty(o, k2, desc);
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, "default", { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o["default"] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+  };
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importStar(require("mongoose"));
 const validator_1 = __importDefault(require("validator"));
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const UserSchema = new mongoose_1.Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide name "],
-        minlength: 3,
-        maxlength: 20,
-        trim: true,
+  name: {
+    type: String,
+    required: [true, "Please provide name "],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email "],
+    validate: {
+      validator: validator_1.default.isEmail,
+      message: "Please provide  valid email",
     },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, "Please provide email "],
-        validate: {
-            validator: validator_1.default.isEmail,
-            message: "Please provide  valid email",
-        },
-    },
-    password: {
-        type: String,
-        required: [true, "Please provide password "],
-        minlength: 6,
-        select: false,
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        maxlength: 20,
-        default: "lastName",
-    },
-    location: {
-        type: String,
-        trim: true,
-        maxlength: 20,
-        default: "my city",
-    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password "],
+    minlength: 6,
+    select: false,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "my city",
+  },
 });
 UserSchema.pre("save", function () {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (!this.isModified("password"))
-            return;
-        if (this.password) {
-            const salt = yield bcryptjs_1.default.genSalt(10);
-            this.password = yield bcryptjs_1.default.hash(this.password, salt);
-        }
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    if (!this.isModified("password")) return;
+    if (this.password) {
+      const salt = yield bcryptjs_1.default.genSalt(10);
+      this.password = yield bcryptjs_1.default.hash(this.password, salt);
+    }
+  });
 });
 UserSchema.methods.createJWT = function () {
-    return jsonwebtoken_1.default.sign({ userId: this._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_LIFETIME,
-    });
+  return jsonwebtoken_1.default.sign(
+    { userId: this._id },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
 };
 UserSchema.methods.comparePassword = function (candidatePassword) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const isMatch = yield bcryptjs_1.default.compare(candidatePassword, this.password);
-        return isMatch;
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    const isMatch = yield bcryptjs_1.default.compare(
+      candidatePassword,
+      this.password
+    );
+    return isMatch;
+  });
 };
 exports.default = mongoose_1.default.model("User", UserSchema);
