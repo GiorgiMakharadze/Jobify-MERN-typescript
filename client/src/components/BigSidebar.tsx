72ef18b56aba40e1cace93a5474e02fd06f39a12
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-04-26 11:55:32 +0400

    Big Sidebar

--- client/src/components/BigSidebar.tsx
+++ client/src/components/BigSidebar.tsx
@@ -1,9 +1,25 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
 
 const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h4>Big Sidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={() => {}} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
